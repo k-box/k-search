bd6828033f3dda0da0237aab11bc1e8b80df445b
Carlos Reig Matut <unstatu@gmail.com>
2017-08-17 11:44:18 +0200

    Implemented the DataHelper isIndexable method

--- src/Helper/DataHelper.php
+++ src/Helper/DataHelper.php
@@ -6,8 +6,10 @@ use App\Model\Data\Data;
 
 class DataHelper
 {
+    const INDEXABLE_TYPES = ['document'];
+
     public function isIndexable(Data $data): bool
     {
-        //@todo: add logic here
+        return in_array($data->type, self::INDEXABLE_TYPES);
     }
 }
