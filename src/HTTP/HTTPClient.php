dfc23244235ac28cf44e8dbad76dda536858e943
Carlos Reig Matut <unstatu@gmail.com>
2017-08-23 16:52:59 +0200

    Implemented a PSR7 Guzzle adapter

---
+++ src/HTTP/HTTPClient.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\HTTP;
+
+use Http\Client\HttpClient as VendorHTTPClient;
+
+interface HTTPClient extends VendorHTTPClient
+{
+    public function requestFile($uri): string;
+}
---
+++ tests/Service/DataDownloaderServiceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\HTTP\GuzzleAdapter;
+use App\Service\DataDownloaderService;
+use App\Tests\Helper\ModelHelper;
+use Http\Client\HttpClient;
+use PHPUnit\Framework\TestCase;
+
+class DataDownloaderServiceTest extends TestCase
+{
+
+    public function testItDownloadsData()
+    {
+        $data = ModelHelper::createDataModel('123');
+        $fileUri = $data->url;
+
+        $guzzle = $this->createMock(GuzzleAdapter::class);
+        $sampleContent = 'this is the file contents';
+        $guzzle->expects($this->once())
+            ->method('requestFile')
+            ->with($fileUri)
+            ->willReturn($sampleContent);
+
+        $downloader = new DataDownloaderService($guzzle);
+        $contents = $downloader->getFileContents($data);
+
+        $this->assertEquals($sampleContent, $contents);
+    }
+}
