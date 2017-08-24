d491d7d97261291bd702db1de6936db2d4b05245
Carlos Reig Matut <unstatu@gmail.com>
2017-08-24 13:48:37 +0200

    Made the DataService public

--- src/Command/DataForProcessingQueueWorkerCommand.php
+++ src/Command/DataForProcessingQueueWorkerCommand.php
@@ -3,28 +3,17 @@
 namespace App\Command;
 
 use App\Service\DataService;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class DataForProcessingQueueWorkerCommand extends Command
+class DataForProcessingQueueWorkerCommand extends ContainerAwareCommand
 {
     /**
      * @var DataService
      */
     private $dataService;
-
-    /**
-     * DataForProcessingQueueWorkerCommand constructor.
-     *
-     * @param mixed $name
-     */
-    public function __construct($name, DataService $dataService)
-    {
-        parent::__construct($name);
-
-        $this->dataService = $dataService;
-    }
 
     protected function configure()
     {
@@ -35,6 +24,7 @@ class DataForProcessingQueueWorkerCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->dataService = $this->getContainer()->get(DataService::class);
         while (true) {
             try {
                 $output->writeln('<info>Polling the queue</info>');
