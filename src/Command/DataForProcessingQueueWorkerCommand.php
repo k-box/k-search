fe671cbe0d93edc1d7eae76baa13042af4e51923
Carlos Reig Matut <unstatu@gmail.com>
2017-08-24 13:51:24 +0200

    Removed the DataService attribute from the DataForProcessingQueueWorkerCommand

--- src/Command/DataForProcessingQueueWorkerCommand.php
+++ src/Command/DataForProcessingQueueWorkerCommand.php
@@ -4,17 +4,11 @@ namespace App\Command;
 
 use App\Service\DataService;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DataForProcessingQueueWorkerCommand extends ContainerAwareCommand
 {
-    /**
-     * @var DataService
-     */
-    private $dataService;
-
     protected function configure()
     {
         $this->setName('ksearch:data-for-processing-queue:worker')
@@ -24,11 +18,12 @@ class DataForProcessingQueueWorkerCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->dataService = $this->getContainer()->get(DataService::class);
+        $dataService = $this->getContainer()->get(DataService::class);
+
         while (true) {
             try {
                 $output->writeln('<info>Polling the queue</info>');
-                if ($this->dataService->processDataFromQueue()) {
+                if ($dataService->processDataFromQueue()) {
                     $output->writeln('<info>Item processed</info>');
                 }
             } catch (\Exception $e) {
