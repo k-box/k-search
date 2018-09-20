7faa7d96c473da1db4ef325a1fc84f3d234c88fc
Alessio <alessio.vertemati@gmail.com>
2018-09-20 20:57:05 +0200

    Code style

--- src/Model/Data/Data.php
+++ src/Model/Data/Data.php
@@ -121,7 +121,7 @@ class Data
 
     /**
      * The Geo location of the data, as an escaped GeoJson string.
-     * 
+     *
      * The coordinates must be in the WGS84 coordinate system.
      * The order of the coordinates must be longitude, latitude
      *
