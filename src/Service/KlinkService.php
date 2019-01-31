e648d1b7b4c0964a29e1cfe1d62d1f466ccc32d1
Alessio <alessio.vertemati@gmail.com>
2019-01-31 12:50:48 +0100

    Add KlinkService to encapsulate klinks logic

---
+++ src/Service/KlinkService.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Log\LoggerInterface;
+use App\Exception\InvalidKlinkException;
+use Symfony\Component\Security\Core\Security;
+use OneOffTech\KLinkRegistryClient\Model\Klink;
+
+class KlinkService
+{
+    /**
+     * @var Security
+     */
+    private $security;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    private $identifiers_cache = null;
+
+    public function __construct(
+        Security $security,
+        LoggerInterface $logger
+    ) {
+        $this->logger = $logger;
+        $this->security = $security;
+    }
+
+    public function someMethod()
+    {
+        // returns User object or null if not authenticated
+        $user = $this->security->getUser();
+
+        $this->logger->error("KlinkService", ['user' => $user]);
+    }
+
+    /**
+     * Get the current authenticated application
+     * 
+     * @return ApiUser
+     */
+    private function application()
+    {
+        return $this->security->getUser();
+    }
+
+    /**
+     * Get current application K-Links
+     * 
+     * @return Klink[]
+     */
+    private function klinks()
+    {
+        $app = $this->application();
+        return $app ? $app->getKlinks() : [];
+    }
+
+    /**
+     * Get the valid K-Link identifier from the current application
+     * 
+     * @return array
+     */
+    private function klinkIdentifiers()
+    {
+        if($this->identifiers_cache){
+            return $this->identifiers_cache;
+        }
+
+        $this->identifiers_cache = array_map(function($k){
+            return $k->getId();
+        }, $this->klinks());
+
+        return $this->identifiers_cache;
+    }
+
+    /**
+     * Get the default K-Link identifier for the
+     * current authenticated application
+     * 
+     * @return string the identifier of the default K-Link for the application
+     * @throws InvalidKlinkException if there are zero K-Links registered to the application or there are more than one K-Link
+     */
+    public function getDefaultKlinkIdentifier()
+    {
+        $klinks = $this->klinks();
+
+        if(empty($klinks) || count($klinks) > 1){
+            throw new InvalidKlinkException("A default K-Link cannot be selected, as the application do not explicity define one");
+        }
+
+        return $klinks[0]->getId();
+    }
+
+    /**
+     * Get the K-Link details that correspond to the specified identifier
+     * 
+     * @param string $identifier The K-Link identifier
+     * @return Klink|null the K-Link details, or null if the identifier does not correspond to a valid K-Link
+     */
+    public function getKlink(string $identifier)
+    {
+        // the application has a list of valid K-Links
+        // we should only found the matching one
+    }
+
+    /**
+     * 
+     * @param array $klinks the identifiers of the K-Links to validate
+     * @param string|Klink $default the default K-Link to return in case the $klinks array is empty
+     * @return array the filtered K-Links to return only the valid ones
+     * @throws InvalidKlinkException if one of the specified K-Link is invalid
+     */
+    public function ensureValidKlinks($klinks, $default = null)
+    {
+        $valid = $this->filterValidKlinks($klinks, $default ?? $this->getDefaultKlinkIdentifier());
+
+        if(!empty($klinks) && count($klinks) !== count($valid)){
+            throw new InvalidKlinkException("Some K-Links are invalid");
+        }
+
+        return $valid;
+    }
+
+    /**
+     * Filter an array of K-Links to return only the one
+     * that the application can see
+     * 
+     * @param array $klinks the identifiers of the K-Links to filter
+     * @param string|Klink $default the default K-Link to return in case the $klinks array is empty
+     * @return array the filtered K-Links to return only the valid ones
+     */
+    public function filterValidKlinks($klinks, $default)
+    {
+        if(empty($klinks)){
+            return [$default];
+        }
+
+        $valid_identifiers = $this->klinkIdentifiers();
+
+        return array_filter($klinks, function($k) use($valid_identifiers){
+            $id = $k instanceof Klink ? $k->getId() : $k;
+            
+            return in_array($k, $valid_identifiers);
+        });
+    }
+}
